a80bc869ba82013b7f94bfc2c4d5f8e927fe25d1
sageli766 <62520805+sageli766@users.noreply.github.com>
2023-10-27 14:45:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,39 @@ from PIL import Image
 
 from pygame.locals import *
 
-input = np.asarray(Image.open('input.png'))
+input = pygame.image.load('input.png')
+
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self):
         super(Tile, self).__init__()
 
-        self.surf = pygame.Surface((input[0], input[1]))
+        self.surf = input
+        self.rect = self.surf.get_rect()
 
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+gameOn = True
+
+tile1 = Tile()
+tile2 = Tile()
+tile3 = Tile()
+tile4 = Tile()
+
+while gameOn:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_BACKSPACE:
+                gameOn = False
+        elif event.type == QUIT:
+            gameOn = False
+
+    screen.blit(tile1.surf, (40, 40))
+    screen.blit(tile2.surf, (40, 530))
+    screen.blit(tile3.surf, (730, 40))
+    screen.blit(tile4.surf, (730, 530))
+
+    pygame.display.flip()
